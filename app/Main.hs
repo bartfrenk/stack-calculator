0df90c290b5f2a96ba0885ad78ffd7c0d84f29d9
Bart Frenk <bart.frenk@gmail.com>
2017-09-17 16:01:03 +0200

    Add command line option to select implementation

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,32 @@ module Main where
 
 import           BasicPrelude
 import           Control.Monad.Managed
+import           Options.Applicative
 
 import qualified Server
 import qualified StackMap
-import qualified StackMap.Impl.STM     as StackMap
+import qualified StackMap.Impl.ConcurrentStack as STACK
+import qualified StackMap.Impl.CTrie           as CTRIE
+import qualified StackMap.Impl.STM             as HAMT_STM
+
+
+data Impl
+  = HAMT_STM
+  | CTRIE
+  | STACK_STM
+
+opts :: ParserInfo Impl
+opts = (helper <*> parser) `info` progDesc "LemonPI stack calculator"
+  where parser =
+          option (eitherReader impl) (
+          short 'i' <>
+          metavar "IMPL" <>
+          help "Which map of stacks implementation to use" <>
+          value HAMT_STM)
+        impl "hamt"  = Right HAMT_STM
+        impl "ctrie" = Right CTRIE
+        impl "stack" = Right STACK_STM
+        impl _       = Left "not an implementation"
 
 
 
@@ -17,12 +39,15 @@ serverC = Server.Config
   , Server.host = "127.0.0.1"
   }
 
-stackMapWithHandle :: (StackMap.Handle Int Double IO -> IO b) -> IO b
-stackMapWithHandle = StackMap.withHandle
+stackMapWithHandle :: Impl -> (StackMap.Handle Int Double IO -> IO b) -> IO b
+stackMapWithHandle HAMT_STM  = HAMT_STM.withHandle
+stackMapWithHandle CTRIE     = CTRIE.withHandle
+stackMapWithHandle STACK_STM = STACK.withHandle
 
 main :: IO ()
-main =
+main = do
+  impl <- execParser opts
   void $ runManaged $ do
-    stackMapH <- managed stackMapWithHandle
+    stackMapH <- managed $ stackMapWithHandle impl
     serverH <- managed $ Server.withHandle serverC stackMapH
     liftIO (Server.runServer serverH)
