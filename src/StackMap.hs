2c52d77b3b91c3b4bcb5525126aa3bb2109cb7c2
Bart Frenk <bart.frenk@gmail.com>
2017-12-08 14:59:06 +0100

    Rename type variables for easier reading

--- src/StackMap.hs
+++ src/StackMap.hs
@@ -1,9 +1,9 @@
 module StackMap where
 
 -- |Abstract interface for an indexed collection of stacks.
-data Handle i a m = Handle
-  { push :: i -> a -> m ()
-  , pop :: i -> m (Maybe a)
-  , size :: i -> m Integer
+data Handle index value m = Handle
+  { push :: index -> value -> m ()
+  , pop :: index -> m (Maybe value)
+  , size :: index -> m Integer
   }
 
